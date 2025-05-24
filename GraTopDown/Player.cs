17a44c9514b4ef1cb0bf82b164bfd9bdcd38b0f9
Jull-ia <panekjuliapv@gmail.com>
2025-05-24 20:55:25 +0200

    reserach

---
+++ GraTopDown/Player.cs
@@ -0,0 +1,11 @@
+class Player
+{
+    public string name;
+    public int x;
+    public int y;
+
+    public Player(string name)
+    {
+        this.name = name;
+    }
+}
--- GraTopDown/Program.cs
+++ GraTopDown/Program.cs
@@ -1,3 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+﻿Console.CursorVisible = false;
+Player hero = new Player("Snake");
+
+Console.WriteLine($"({hero.x}, {hero.y})");
+
+while (true)
+{
+    ConsoleKeyInfo pressedKeyInfo = Console.ReadKey(true);
+
+    if (pressedKeyInfo.Key == ConsoleKey.A)
+    {
+        hero.x -= 1;
+    }
+    if (pressedKeyInfo.Key == ConsoleKey.D)
+    {
+        hero.x += 1;
+    }
+    if (pressedKeyInfo.Key == ConsoleKey.W)
+    {
+        hero.y -= 1;
+    }
+    if (pressedKeyInfo.Key == ConsoleKey.S)
+    {
+        hero.y += 1;
+    }
+
+    Console.SetCursorPosition(0, 0);
+    Console.WriteLine($"({hero.x}, {hero.y})");
+
+    Console.SetCursorPosition(hero.x, hero.y);
+    Console.Write("@");
+}
+
+Console.WriteLine("Press Space to continue...");
+ConsoleKeyInfo consoleKeyInfo;
+
+do
+{
+    consoleKeyInfo = Console.ReadKey(true);
+} while (consoleKeyInfo.Key != ConsoleKey.Spacebar);
+
+
+/*
+Player - klasa
+ 
+hp - liczba,
+maxhp (domyslnie 100),
+stamina,
+maxstamina,
+name - tekst,
+speed,
+
+
+Konrad - obiket typu gracz
+ 
+hp - 100,
+maxhp - 100,
+stamina - 20-max stamina - 20,
+name - Konrad,
+
+ 
+Gremlin - obiket typu gracz
+ 
+hp - 20,
+maxhp - 50,
+stamina - 120-max stamina - 120,
+name - Gremlin,
+*/
