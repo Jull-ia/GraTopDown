7f923808508ccaf7a26b6c31239978ea9e469b5b
Jull-ia <panekjuliapv@gmail.com>
2025-06-14 14:14:15 +0200

    aktualizacja mapy

--- GraTopDown/Player.cs
+++ GraTopDown/Player.cs
@@ -1,17 +1,3 @@
-// class Player
-// {
-//     public string name;
-//     public int x;
-//     public int y;
-
-//     public Player(string name)
-//     {
-//         this.name = name;
-//     }
-// }
-
-
-// using System.Drawing;
 
 // class Player : Character
 // {
